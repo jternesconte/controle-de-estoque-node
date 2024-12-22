c76a394849963b2f1fb3da0c61b1095565d6d7be
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-12-22 17:08:38 -0300

    feat: adds Saida controller for all routes

--- src/controllers/SaidaController.ts
+++ src/controllers/SaidaController.ts
@@ -1,3 +1,44 @@
+import { Request, Response } from 'express';
+import { produtoRepository } from '../repositories/ProdutoRepository';
+import { saidaRepository } from '../repositories/SaidaRepository';
+
 export class SaidaController {
 
+   // nova saida
+   async newSaida(req: Request, res: Response) {
+      try {
+         const { quantidade } = req.body;
+         const { produtoId } = req.params
+
+         const produto = await produtoRepository.findOneBy({ id: Number(produtoId) });
+         if(!produto) {
+            res.status(404).json({ error: 'Produto não encontrado com o id: ' + produtoId });
+            return;
+         }
+
+         produto.quantidade = produto.quantidade - Number(quantidade);
+
+         await produtoRepository.save(produto);
+
+         const newSaida = saidaRepository.create({ produto, quantidade });
+
+
+         saidaRepository.save(newSaida);
+
+         res.status(200).json(newSaida);
+      } catch (error) {
+         res.status(500).json({ error: 'Erro ao gerar saída' });
+      }
+   }
+
+   // get em todas as saidas
+   async getSaidas(req: Request, res: Response) {
+      try {
+         const saidas = await saidaRepository.find();
+
+         res.status(200).json(saidas);
+      } catch (error) {
+         res.status(500).json({ error: 'Erro ao buscar saídas' });
+      }
+   }
 }
