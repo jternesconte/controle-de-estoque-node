2e13d74e2a03d9ffcf9e036448fffaa89211bc0a
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-12-22 17:08:31 -0300

    feat: adds Entrada controller for all routes

--- src/controllers/EntradaController.ts
+++ src/controllers/EntradaController.ts
@@ -1,3 +1,44 @@
+import { Request, Response } from 'express';
+import { produtoRepository } from '../repositories/ProdutoRepository';
+import { entradaRepository } from '../repositories/EntradaRepository';
+
 export class EntradaController {
 
+   // nova entrada
+   async newEntrada(req: Request, res: Response) {
+      try {
+         const { quantidade } = req.body;
+         const { produtoId } = req.params
+
+         const produto = await produtoRepository.findOneBy({ id: Number(produtoId) });
+         if(!produto) {
+            res.status(404).json({ error: 'Produto não encontrado com o id: ' + produtoId });
+            return;
+         }
+
+         produto.quantidade = produto.quantidade + Number(quantidade);
+
+         await produtoRepository.save(produto);
+
+         const newEntrada = entradaRepository.create({ produto, quantidade });
+
+
+         entradaRepository.save(newEntrada);
+
+         res.status(200).json(newEntrada);
+      } catch (error) {
+         res.status(500).json({ error: 'Erro ao gerar entrada' });
+      }
+   }
+
+   // get em todas as entradas
+   async getEntradas(req: Request, res: Response) {
+      try {
+         const entradas = await entradaRepository.find();
+
+         res.status(200).json(entradas);
+      } catch (error) {
+         res.status(500).json({ error: 'Erro ao buscar entradas' });
+      }
+   }
 }
