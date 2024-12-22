5a675a02ed7196a9a8e92813ff6c646a8d5c17a7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-12-22 17:09:08 -0300

    feat: implements Saida routes

--- src/routes/SaidaRoutes.ts
+++ src/routes/SaidaRoutes.ts
@@ -3,8 +3,8 @@ import { SaidaController } from "../controllers/SaidaController";
 
 export const saidaRoutes = Router();
 
-/* // Rota para obter todas as saídas
-saidaRoutes.get('/getAll', new SaidaController().getAllSaidas);
+// Rota para obter todas as saídas
+saidaRoutes.get('/getAll', new SaidaController().getSaidas);
 
 // Rota para adicionar uma nova saída
-saidaRoutes.post('/newSaida/:produtoId', new SaidaController().novaSaida); */
+saidaRoutes.post('/newSaida/:produtoId', new SaidaController().newSaida);
