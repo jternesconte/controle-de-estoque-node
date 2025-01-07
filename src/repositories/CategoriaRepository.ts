96f1465a81b025fa138c31ae61c97355d1b0ac36
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-06 21:13:44 -0300

    feat: implements a save method for save with a Interface of entity

--- src/repositories/CategoriaRepository.ts
+++ src/repositories/CategoriaRepository.ts
@@ -1,4 +1,16 @@
 import { AppDataSource } from '../data-source';
+import { ICategoria } from '../interfaces/ICategoria';
 import { Categoria } from './../entities/Categoria';
 
-export const categoriaRepository = AppDataSource.getRepository(Categoria);
+export const categoriaRepository = AppDataSource.getRepository(Categoria).extend({
+   async saveCategoria(data: ICategoria): Promise<Categoria> {
+      const categoria = this.create({
+         nome: data.nome,
+         descricao: data.descricao,
+         flAtivo: data.flAtivo,
+      });
+      return this.save(categoria);
+   }
+});
+
+   
