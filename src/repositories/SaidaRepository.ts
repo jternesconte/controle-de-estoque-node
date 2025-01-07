b6caf73729b51ee8cb1fd4468556b6fd86b7ab78
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-06 21:13:50 -0300

    feat: implements a save method for save with a Interface of entity

--- src/repositories/SaidaRepository.ts
+++ src/repositories/SaidaRepository.ts
@@ -1,4 +1,13 @@
 import { AppDataSource } from "../data-source";
 import { Saida } from "../entities/Saida";
+import { ISaida } from "../interfaces/ISaida";
 
-export const saidaRepository = AppDataSource.getRepository(Saida);
+export const saidaRepository = AppDataSource.getRepository(Saida).extend({
+   async saveSaida(data: ISaida): Promise<Saida> {
+         const saida = this.create({
+            produto: data.produto,
+            quantidade: data.quantidade,
+         });
+         return this.save(saida);
+      }
+});
