5dd18acdd484b43f73a3f39a4c523be6ad53403e
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-06 21:13:39 -0300

    feat: implements a save method for save with a Interface of entity

--- src/repositories/ProdutoRepository.ts
+++ src/repositories/ProdutoRepository.ts
@@ -1,4 +1,17 @@
 import { AppDataSource } from '../data-source';
+import { IProduto } from '../interfaces/IProduto';
 import { Produto } from './../entities/Produto';
 
-export const produtoRepository = AppDataSource.getRepository(Produto);
+export const produtoRepository = AppDataSource.getRepository(Produto).extend({
+   async saveProduto(data: IProduto): Promise<Produto> {
+      const produto = this.create({
+         nome: data.nome,
+         descricao: data.descricao,
+         preco: data.preco,
+         quantidade: data.quantidade,
+         categoria: data.categoria,
+         flAtivo: data.flAtivo,
+      });
+      return this.save(produto);
+   }
+});
