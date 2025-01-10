65f9e626632ea0265568461ae0ee0c9ac0f00421
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-10 13:08:29 -0300

    feat: implements a user register method

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -1,10 +1,42 @@
 import { Request, Response } from 'express';
+import { usuarioRepository } from '../repositories/UsuarioRepository';
+import { IUsuario } from '../interfaces/IUsuario';
+
+const bcrypt = require("bcrypt");
 
 export class AuthenticationController {
 
+
    // novo cadastro
    async newCadastro(req: Request, res: Response) {
-      
+      try {
+         const { nome, email, senha, senha2 } = req.body;
+
+         if(senha !== senha2) {
+            res.status(404).json({ error: 'Confirmação de senha não está igual' });
+            return;
+         }
+
+         const usuarioExistente = await usuarioRepository.findOneBy({ email: email });
+         if(usuarioExistente) {
+            res.status(404).json({ error: 'Email já cadastrado' });
+            return;
+         }
+
+         const senhaHashed = await bcrypt.hash(senha, 10);
+
+         const newUsuario: IUsuario = {
+            nome,
+            email,
+            senha: senhaHashed
+         };
+
+         await usuarioRepository.save(newUsuario);
+
+         res.status(201).json(newUsuario);
+      } catch {
+         res.status(500).json({ error: 'Erro ao adicionar novo usuário' });
+      }
    }
 
    // realizar login
