f6953fb5374601598dfc6506b1f66e10e61dce99
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-09 22:23:10 -0300

    feat: adds authentication Controller

---
+++ src/controllers/AuthenticationController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+export class AuthenticationController {
+
+   // novo cadastro
+   async newCadastro(req: Request, res: Response) {
+      
+   }
+
+   // realizar login
+   async userLogin(req: Request, res: Response) {
+      
+   }
+}
