f8d75ff09115d8344421bfa802f82165fc009b06
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-14 13:11:16 -0300

    feat: implements authentication to Entrada routes

--- src/routes/EntradaRoutes.ts
+++ src/routes/EntradaRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { EntradaController } from "../controllers/EntradaController";
+import { authenticateToken } from "../middlewares/authenticateToken";
 
 export const entradaRoutes = Router();
+
+entradaRoutes.use(authenticateToken);
 
 // Rota para obter todas as entradas
 entradaRoutes.get('/getAll', new EntradaController().getEntradas);
