b2c1315e80699f4db7054c9d8f821ad4dd24551f
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-14 13:11:00 -0300

    feat: implements authentication to Saida routes

--- src/routes/SaidaRoutes.ts
+++ src/routes/SaidaRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { SaidaController } from "../controllers/SaidaController";
+import { authenticateToken } from "../middlewares/authenticateToken";
 
 export const saidaRoutes = Router();
+
+saidaRoutes.use(authenticateToken);
 
 // Rota para obter todas as saídas
 saidaRoutes.get('/getAll', new SaidaController().getSaidas);
