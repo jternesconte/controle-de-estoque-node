3948dcec0aaf79df9283d084a2a2dde356ea43f7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-13 22:16:02 -0300

    fix: removes return of user data for the requisition (data leak)

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { usuarioRepository } from '../repositories/UsuarioRepository';
 import { IUsuario } from '../interfaces/IUsuario';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class AuthenticationController {
 
@@ -11,8 +12,13 @@ export class AuthenticationController {
       try {
          const { nome, email, senha, senha2 } = req.body;
 
+         if(!nome) {
+            res.status(404).json({ error: "O campo 'nome' é obrigatório " });
+            return;
+         }
+
          if(senha !== senha2) {
-            res.status(404).json({ error: 'Confirmação de senha não está igual' });
+            res.status(404).json({ error: 'Confirmação de senha não é válida' });
             return;
          }
 
@@ -32,7 +38,7 @@ export class AuthenticationController {
 
          await usuarioRepository.save(newUsuario);
 
-         res.status(201).json(newUsuario);
+         res.status(201).json('Usuário registrado com sucesso');
       } catch {
          res.status(500).json({ error: 'Erro ao adicionar novo usuário' });
       }
@@ -47,16 +53,22 @@ export class AuthenticationController {
          if(usuarioExistente) {
             const isSenhaCombina: boolean = await bcrypt.compare(senha, usuarioExistente.senha);
 
-            if(isSenhaCombina) {
-               res.status(201).json('logou');
-            } else {
+            if(!isSenhaCombina) {
                res.status(404).json({ error: 'Senha inválida' });
             }
+
          } else {
             res.status(404).json({ error: 'Email não cadastrado' });
             return;
          }
+         
+         const secret = process.env.SECRET_KEY as string;
 
+         const token = jwt.sign({ userId: usuarioExistente.id }, secret, {
+            expiresIn:'1h'
+         });
+
+         res.status(201).json({ msg: 'Autenticação realizada com sucesso', token });
       } catch {
          res.status(500).json({ error: 'Erro ao realizar login' })
       }
