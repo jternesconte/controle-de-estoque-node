07f072a6a90943fd1514f47a8137ebc5679955a9
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-15 20:11:58 -0300

    improvement: adds authenticate middleware to editUser route

--- src/routes/AuthenticationRoutes.ts
+++ src/routes/AuthenticationRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthenticationController } from "../controllers/AuthenticationController";
+import { authenticateToken } from "../middlewares/authenticateToken";
 
 export const authenticationRoutes = Router();
 
@@ -10,4 +11,4 @@ authenticationRoutes.post('/register', new AuthenticationController().newCadastr
 authenticationRoutes.post('/login', new AuthenticationController().userLogin);
 
 //Rota para editar usuario
-authenticationRoutes.put('/editUser', new AuthenticationController().editUser);
+authenticationRoutes.put('/editUser', authenticateToken, new AuthenticationController().editUser);
