a42abe2a964c7116811213ab955ac3be24ea850c
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-15 20:13:26 -0300

    improvement: adds usuario to Saida entity

--- src/entities/Entrada.ts
+++ src/entities/Entrada.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, Column, CreateDateColumn } from "typeorm";
 import { Produto } from "./Produto";
+import { Usuario } from "./Usuario";
 
 @Entity("entradas")
 export class Entrada {
@@ -9,6 +10,10 @@ export class Entrada {
   @ManyToOne(() => Produto, { nullable: false })
   @JoinColumn({ name: "produto_id", referencedColumnName: 'id' })
   produto: Produto;
+
+  @ManyToOne(() => Usuario, { nullable: false })
+  @JoinColumn({ name: "usuario_id", referencedColumnName: 'id' })
+  usuario: Usuario;
 
   @Column({ type: "int", nullable: false })
   quantidade: number;
